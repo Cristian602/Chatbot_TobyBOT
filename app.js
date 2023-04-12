31be9503fc704b19cf06a69fa41fe4593b947a2d
Cristian Mendez <ricardo.mendez@ucuenca.edu.ec>
2023-04-12 13:08:36 -0500

    feat: Recibir mensajes de cualquier usuario de WhatsApp

---
+++ app.js
@@ -0,0 +1,26 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    client
+        .sendText(message.from, 'Bienvenido a TobyBot. Estoy listo para responder tus dudas sobre procesos de titulación de la Facultad de Economía')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+  });
+}
